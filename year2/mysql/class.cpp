8972b40f0bce435972f505c6e02aaa8a111c0567
karthik <karthikoggu2@outlook.com>
2019-12-30 00:49:52 +0530

    tst

---
+++ year2/mysql/class.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class AnyName
+{
+    public :                            //Access specifier.
+        string input;                   //Data Member.
+    void printname()                    //Member Function.
+    {
+        cout<< "Input is : "<<input;
+    }
+};
+int main()
+{
+    AnyName object1;                    //Objcet Declaration.
+    object1.input = "Karthik.";         //Assignig DataMember.
+    object1.printname();                //Using Member function.
+    return 0;
+}
