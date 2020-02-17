950d0261f4a689403474d6ae7f4acdc4606f0627
karthikone <karthikoggu2@outlook.com>
2020-02-17 23:31:03 +0530

    hello

--- year2/java/hello.java
+++ year2/java/hello.java
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 class hello {
     public static void main( String []args ) {
         System.out.println( "Hello World!" );
     }
 }
-=======
-test test
-
->>>>>>> 188cdb2b878e5c719a80e963f45d83336022ce73
