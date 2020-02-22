8cd81bb6ca116a0a313ba7a8b1a280549f27fe50
Karthikone <karthikoggu2@outlook.com>
2020-02-23 02:02:34 +0530

    2dstrings.c

--- year1/pps/2dstrings.c
+++ year1/pps/2dstrings.c
@@ -5,5 +5,4 @@ int main()
     char string[6][10]={"apple","microsoft","spacex","facebook","bitcoin"};//here [6] is rows and [10] coloumns.
     for(i=0;i<5;i++)
         puts(string[i]);
-    //testing5
 }
