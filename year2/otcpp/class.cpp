0a0d2b64937d9865c2b3243389f50debb9d364e1
Karthikone <karthikoggu2@outlook.com>
2020-02-23 01:52:37 +0530

    class.cpp

--- year2/otcpp/class.cpp
+++ year2/otcpp/class.cpp
@@ -12,7 +12,7 @@ class AnyName
 int main()
 {
     AnyName object1;                    //Objcet Declaration.
-    object1.input = "Karthik.";         //Assignig DataMember.
+    object1.input = "Karthik.\n";         //Assignig DataMember.
     object1.printname();                //Using Member function.
     return 0;
 }
