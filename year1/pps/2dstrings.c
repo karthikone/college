dbb84c16b36028358634708e4d79448645347b58
Karthikone <karthikoggu2@outlook.com>
2020-02-23 14:30:40 +0530

    nothing changed

--- year1/pps/2dstrings.c
+++ year1/pps/2dstrings.c
@@ -4,5 +4,5 @@ int main()
     int i;
     char string[6][10]={"apple","microsoft","spacex","facebook","bitcoin"};//here [6] is rows and [10] coloumns.
     for(i=0;i<5;i++)
-        puts(string[i]);
+        puts(string[i]) ;
 }
