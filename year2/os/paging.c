12db68ea8d2ea54071a40e1c248a00c394108147
Karthikone <karthikoggu2@outlook.com>
2020-03-09 04:41:37 +0530

    paging.c

---
+++ year2/os/paging.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int ms,ps,nop,np,rempages,i,j,x,y,pa,offset,s[10],pno[10][20];
+    printf("Enter the memory size : ");
+    scanf("%d",&ms);
+    printf("\nEnter the page Size : ");
+    scanf("%d",&ps);
+    nop = ms/ps;
+    printf("\nNumber of pages avilable in memory : %d",nop);
+    //some code is missing.
+    printf("\nEnter Number of processes : ");
+
+}
