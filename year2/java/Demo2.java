35686d6cf235c6a47c5ee9b840607fe670b62cde
Karthikone <karthikoggu2@outlook.com>
2020-03-09 15:20:27 +0530

    uncompleted

---
+++ year2/java/Demo2.java
@@ -0,0 +1,29 @@
+class person
+{
+    String name;
+    int age;
+    person()
+    {
+        name = "Karthik";
+        age = 20;
+    }
+    person(String , i)
+    {
+        name = s;
+         age = i;
+    }
+    void talk()
+    {
+        system.out.println("Hello I'm "+name);
+        system.out.println("My age : "+age);
+    }
+}
+class Demo2.java
+{
+    public static void main(String args[])
+    {
+        person Karhtik = new person();
+        Karhtik.tlak();
+        person Karthik = new person("String")
+    }
+}
