34a7f482129e2fd80ed878ea1450f559feb8e458
karthik <karthikoggu2@outlook.com>
2020-03-26 22:08:16 +0530

    more new files

--- year2/os/paging.c
+++ year2/os/paging.c
@@ -1,14 +1,27 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int ms,ps,nop,np,rempages,i,j,x,y,pa,offset,s[10],pno[10][20];
-    printf("Enter the memory size : ");
-    scanf("%d",&ms);
-    printf("\nEnter the page Size : ");
-    scanf("%d",&ps);
-    nop = ms/ps;
-    printf("\nNumber of pages avilable in memory : %d",nop);
-    //some code is missing.
-    printf("\nEnter Number of processes : ");
-
+int size,m,n,pgno,pagetable[3]={5,6,7},i,j,frameno;
+double m1;
+int ra=0,ofs;
+printf("Enter process size (in KB of max 12KB):");/*reading memeory size*/
+scanf("%d",&size);
+m1=size/4;
+n=ceil(m1);
+printf("Total No. of pages: %d",n);
+printf("\nEnter relative address (in hexadecimal notation eg.0XRA) \n");
+//printf("The length of relative Address is : 16 bits \n\n The size of offset is :12bits\n");
+scanf("%d",&ra);
+pgno=ra/1000;
+/*calculating physical address*/
+ofs=ra%1000;
+printf("page no=%d\n",pgno);
+printf("page table");
+for(i=0;i<n;i++)
+printf("\n %d [%d]",i,pagetable[i]);
+frameno=pagetable[pgno];
+printf("\n Equivalent physical address : %d%d",frameno,ofs);
+return 0;
 }
+
